7ab558049c6f153e2e6316b655e9ac64665038ec
Alexander Henka <Alexander.Henka@googlemail.com>
2015-11-19 19:18:48 +0100

    This Test

--- this/this_is_it.js
+++ this/this_is_it.js
@@ -1,3 +1,53 @@
 /**
  * Created by alex on 17.11.15.
  */
+
+
+
+var  a = 2;
+
+function foo(a){
+
+    console.log(a +1);
+
+
+//    var _a = 4 ;
+
+}
+
+
+function foo(a,b){
+
+    console.log('b = ' + b);
+
+    console.log(a +b+1);
+
+
+//    var _a = 4 ;
+
+}
+
+
+var obj = {
+    a : 33,
+    foo: foo
+};
+
+
+var myObj = function(f){
+
+    var foo = f || "default";
+
+    console.log('sdsdd' +foo);
+
+
+};
+
+
+
+new myObj("hjhhjhj");
+
+
+obj.foo();
+
+//foo(1);
